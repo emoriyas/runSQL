144e31af3bb9b96f35e6206a3444dedcfa466a1a
emoriyas <ehddnr85963@gmail.com>
2017-02-02 15:50:38 -1000

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,14 @@ import java.sql.Statement;
 //import java.sql.ResultSetMetaData;
 
 /**
- * Author: Eric Moriyasu ICS 421 Assignment 1 runDLL Jan. 23 2017
+ * Author: Eric Moriyasu ICS 421 Assignment 1 
+ * 
+ * reads from clustercfg and ddlfile to run a multi threaded ddl queries
+ * 
+ * Jan. 23 2017
  */
+
+
 public class Main {
 
 	// varaibles to hold clustercfg data
@@ -30,6 +36,19 @@ public class Main {
 
 	static int numnodes = -1;
 
+	
+	/* Main method
+	 * Parameter: (String[]) args : contains filename. First slot is the
+	 * 								clustercfg, second slot is the ddlfile
+	 * 
+	 * Description: takes two file names, clustercfg which contains the
+	 * 				information on cluster nodes and ddlfile that contains
+	 * 				the sql queries. Main method will call functions to
+	 * 				read informations on the files and write the necessary
+	 * 				informations. 
+	 * 
+	 * Returns: void
+	*/
 	public static void main(String[] args) throws InterruptedException {
 
 		String clustercfg = "";
@@ -37,9 +56,6 @@ public class Main {
 		String line = null;
 		String wordLeft = ""; // left side of catalog eqs
 		String wordRight = ""; // right side of catalog eqs
-		//SimpleThreads thread = new SimpleThreads();
-
-		// ArrayList<String> sqlAL = new ArrayList<String>();
 
 		// ArrayList to store the node information
 		// ArrayList<DBNode> nodeAL = new ArrayList<DBNode>();
@@ -139,24 +155,25 @@ public class Main {
 					// catalog = -1;
 				} // end of while
 
-				//String tname = "";
-				
+				// String tname = "";
+
 				readDDL(ddlfile);
-				
+
 				/*
-				String derp = "CREATE TABLE BOOKS(isbn char(14), title char(80), price decimal)";
-				
-				System.out.println(derp);
-				System.out.print("herr drr: ");
-				System.out.println(parseTname(derp));
-				*/
-				
+				 * String derp =
+				 * "CREATE TABLE BOOKS(isbn char(14), title char(80), price decimal)"
+				 * ;
+				 * 
+				 * System.out.println(derp); System.out.print("herr drr: ");
+				 * System.out.println(parseTname(derp));
+				 */
+
 				doThread();
-				
-				if (successful){ 
+
+				if (successful) {
 					runCatalog();
 				}
-				
+
 			} catch (IOException e) {
 				System.out.println("File not found");
 			}
@@ -165,32 +182,21 @@ public class Main {
 			System.out.println("Argument insufficient");
 		}
 
-		/*
-		 * System.out.println("\n");
-		 * 
-		 * //System.out.println(clustercfg + ddlfile); System.out.println(
-		 * "Catalog Driver: " + catalogDriver); System.out.println(
-		 * "Catalog HN: " + catalogHostName); System.out.println("Catalog UN: "
-		 * + catalogUserName); System.out.println("Catalog Pass: " +
-		 * catalogPassword);
-		 * 
-		 * System.out.println("Number of nodes: " + numnodes);
-		 * 
-		 * System.out.println("nodeAL size: " + nodeAL.size());
-		 * 
-		 * for (int x = 0; x < nodeAL.size();x++) { System.out.println(
-		 * "Node Driver: " + nodeAL.get(x).getDriver()); System.out.println(
-		 * "Node HN: " + nodeAL.get(x).getHostName()); System.out.println(
-		 * "NOde UN: " + nodeAL.get(x).getUserName()); System.out.println(
-		 * "node Pass: " + nodeAL.get(x).getPassword()); }
-		 */
-
-		// readDDL(ddlfile, sqlAL);
-
 	}// end of main
 
+	
+	/* Function processCatalogLine
+	 * Parameter: (String) line : line to be processed
+	 * 
+	 * Description: takes a line of String read from clustercfg.
+	 * 				It takes the necessary information and passes the
+	 * 				catalog values to global variables so the program
+	 * 				can run updates to the catalog DB
+	 * 
+	 * Returns: void
+	*/
 	private static void processCatalogLine(String line) {
-		
+
 		String wordLeft = ""; // left side of catalog eqs
 
 		/*
@@ -226,6 +232,17 @@ public class Main {
 
 	} // end of method
 
+	/* Function catalogCompare 
+	 * Parameter: (String) line : String to be processed
+	 * 
+	 * Description: Takes a string and compares it to a set of strings and
+	 * 				returns a integer value based on the matches. 
+	 * 
+	 * Returns: (int) : if 0: the processCatalogLine function will look	for a catalog driver
+	 * 					if 1: the processCatalogLine function will look for a catalog hostname						
+	 * 					if 2: the processCatalogLine function will look for a catalog username	
+	 * 					if 3: the processCatalogLine function will look for a catalog password	
+	*/
 	private static int catalogCompare(String line) {
 
 		int ret = -1;
@@ -245,6 +262,10 @@ public class Main {
 		return (ret);
 	}
 
+	
+	/*
+	 This function is currently not in use	
+	*/
 	private static void processNodeLine(String line, int node) {
 
 		String wordLeft = ""; // left side of catalog eqs
@@ -282,6 +303,16 @@ public class Main {
 
 	}
 
+	
+	/* Function readDDL
+	 * Parameter: (String) ddl : filename of the ddlfile
+	 * 
+	 * Description: Takes a ddlfilename and gets the query information.
+	 * 				the function will push the queries into sqlAL for
+	 * 				future use. 
+	 * 
+	 * Returns: void
+	*/
 	// private static void readDDL(String ddl, ArrayList<String> arrayList) {
 	private static void readDDL(String ddl) {
 		String line = "";
@@ -314,6 +345,18 @@ public class Main {
 		// return(sql);
 	}
 
+	/* Function runDDL
+	 * Parameter: (String) JDBC_DRIVER : driver for the database node
+	 * 				(String) DB_URL : url/hostname for the database node
+	 * 				(String) USER : username for the database node
+	 * 				(String) PASS : password for the database node
+	 * 				(String) sql : sql to be executed
+	 * 
+	 * Description: attempts to establish a datbase connection and run a
+	 * 				sql query. Will close connection after query is completed
+	 * 
+	 * Returns: void
+	*/
 	private static void runDDL(String JDBC_DRIVER, String DB_URL, String USER, String PASS, String sql) {
 
 		Connection conn = null;
@@ -325,53 +368,48 @@ public class Main {
 
 			// STEP 3: Open a connection
 			System.out.println("Connecting to database...");
-			
-			if(USER.equals(" ") && PASS.equals(" ")) {
+
+			if (USER.equals(" ") && PASS.equals(" ")) {
 				conn = DriverManager.getConnection(DB_URL);
-			}
-			else {
+			} else {
 				conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			}
-			
-			//conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			//conn = DriverManager.getConnection(DB_URL);
+
+			// conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			// conn = DriverManager.getConnection(DB_URL);
 
 			// STEP 4: Execute a query
 			System.out.println("Creating statement...");
 			stmt = conn.createStatement();
 
-			//ResultSet rs = null;
+			// ResultSet rs = null;
 			stmt.executeUpdate(sql);
-			//stmt.executeQuery(sql);
+			// stmt.executeQuery(sql);
 
 			// STEP 5: Extract data from result set
-			
+
 			/*
-			while (rs.next()) {
-				// Retrieve by column name
-				//int id = rs.getInt("id");
-				//String name = rs.getString("name");
-				
-				// Display values
-				//System.out.print("ID: " + id);
-				//System.out.println(", name: " + name);
-			}
-			*/
-			
-			System.out.println("[" + DB_URL.substring(0, (DB_URL.length() -1 )) + "]: sql success");
+			 * while (rs.next()) { // Retrieve by column name //int id =
+			 * rs.getInt("id"); //String name = rs.getString("name");
+			 * 
+			 * // Display values //System.out.print("ID: " + id);
+			 * //System.out.println(", name: " + name); }
+			 */
+
+			System.out.println("[" + DB_URL.substring(0, (DB_URL.length() - 1)) + "]: sql success");
 			successful = true;
-			
-			//rs.close();
+
+			// rs.close();
 			stmt.close();
 			conn.close();
 		} catch (SQLException se) {
 			// Handle errors for JDBC
-			//se.printStackTrace();
-			System.out.println("[" + DB_URL.substring(0, (DB_URL.length() -1 )) + "]: sql failed");
+			// se.printStackTrace();
+			System.out.println("[" + DB_URL.substring(0, (DB_URL.length() - 1)) + "]: sql failed");
 		} catch (Exception e) {
 			// Handle errors for Class.forName
-			//e.printStackTrace();
-			System.out.println("[" + DB_URL.substring(0, (DB_URL.length() -1 )) + "]: sql failed");
+			// e.printStackTrace();
+			System.out.println("[" + DB_URL.substring(0, (DB_URL.length() - 1)) + "]: sql failed");
 		} finally {
 			// finally block used to close resources
 			try {
@@ -386,28 +424,38 @@ public class Main {
 				se.printStackTrace();
 			} // end finally try
 		} // end try
-		//System.out.println("Goodbye!");
+			// System.out.println("Goodbye!");
 
 	}
+
 	
-	private static void runCatalog() {	
-		//runDDL(String JDBC_DRIVER, String DB_URL, String USER, String PASS, String sql)
+	/* Function runCatalog
+	 * Parameter: none
+	 * 
+	 * Description: attempts to establish a database connection to the catalog DB
+	 * 				and run update to metadata table. Will close connection after 
+	 * 				update is completed
+	 * 
+	 * Returns: void
+	*/
+	private static void runCatalog() {
+		// runDDL(String JDBC_DRIVER, String DB_URL, String USER, String PASS,
+		// String sql)
 
 		System.out.println(sqlAL.get(0));
-		
+
 		/*
-		static String catalogDriver = "";
-		static String catalogHostName = "";
-		static String catalogUserName = "";
-		static String catalogPassword = "";
-		*/
+		 * static String catalogDriver = ""; static String catalogHostName = "";
+		 * static String catalogUserName = ""; static String catalogPassword =
+		 * "";
+		 */
 		Connection conn = null;
 		Statement stmt = null;
 		String sql = "";
 		boolean tableExist = true;
 		String tname = "";
 		String cmd = "";
-			
+
 		try {
 			// STEP 2: Register JDBC driver
 			Class.forName(catalogDriver);
@@ -417,107 +465,91 @@ public class Main {
 
 			// STEP 4: Execute a query
 			stmt = conn.createStatement();
-									
+
 			try {
-				//String tname, String JDBC_DRIVER, String DB_URL, String USER, String PASS, int nodeid
-				
+				// String tname, String JDBC_DRIVER, String DB_URL, String USER,
+				// String PASS, int nodeid
+
 				sql = "SELECT * FROM dtables";
 				stmt.executeQuery(sql);
-				
+
 				System.out.println("Catalog table already exists");
-				
-			} catch( SQLException e ) {
-				
-			    tableExist = false;
-			    
-			    System.out.println("Catalog table does not exist");
+
+			} catch (SQLException e) {
+
+				tableExist = false;
+
+				System.out.println("Catalog table does not exist");
 			}
-									
-			if(tableExist == false) {
-				sql = "CREATE TABLE dtables(tname char(32),"
-						+ "nodedriver char(64),"
-						+ "nodeurl char(128),"
-						+ "nodeuser char(16)," 
-						+ "nodepasswd char(16)," 
-						+ "partmtd int," 
-						+ "nodeid int,"
-						+ "partcol char(32)," 
-						+ "partparam1 char(32),"
-						+ "partparam2 char(32))";
+
+			if (tableExist == false) {
+				sql = "CREATE TABLE dtables(tname char(32)," + "nodedriver char(64)," + "nodeurl char(128),"
+						+ "nodeuser char(16)," + "nodepasswd char(16)," + "partmtd int," + "nodeid int,"
+						+ "partcol char(32)," + "partparam1 char(32)," + "partparam2 char(32))";
 
 				stmt.executeUpdate(sql);
-				
+
 				System.out.println("SQL table created");
 			}
-			
+
 			/*
-			 INSERT INTO SECONDTABLE VALUES 
-    (100,'ONE HUNDRED'),(200,'TWO HUNDRED'),(300,'THREE HUNDRED');
-    
-(String JDBC_DRIVER, String DB_URL, String USER, String PASS, String sql)    
+			 * INSERT INTO SECONDTABLE VALUES (100,'ONE HUNDRED'),(200,'TWO
+			 * HUNDRED'),(300,'THREE HUNDRED');
+			 * 
+			 * (String JDBC_DRIVER, String DB_URL, String USER, String PASS,
+			 * String sql)
 			 */
-			
-			
-			for (int x  = 0; x < nodeAL.size() - 1;x++) {
-				
-				
+
+			for (int x = 0; x < nodeAL.size() - 1; x++) {
+
 				tname = parseTname(sqlAL.get(x));
-				
+
 				cmd = getCmd(sqlAL.get(x));
-				//System.out.println(cmd);
-				
-				if (cmd.equals("CREATE")){
-					
-					sql = "INSERT INTO dtables VALUES "
-							+ "('" + tname + "', "
-							+ "'" + (nodeAL.get(x)).getDriver() + "', "
-							+ "'" + (nodeAL.get(x)).getHostName() + "', "
-							+ "'" + (nodeAL.get(x)).getUserName() + "', "
-							+ "'" + (nodeAL.get(x)).getPassword() + "', "
-							+ -1 + ", "
-							+ "" + x + ", "
-							+ null + ", "
-							+ null + ", "
-							+ null
-							+ ")";
-					
-				
+				// System.out.println(cmd);
+
+				if (cmd.equals("CREATE")) {
+
+					sql = "INSERT INTO dtables VALUES " + "('" + tname + "', " + "'" + (nodeAL.get(x)).getDriver()
+							+ "', " + "'" + (nodeAL.get(x)).getHostName() + "', " + "'" + (nodeAL.get(x)).getUserName()
+							+ "', " + "'" + (nodeAL.get(x)).getPassword() + "', " + -1 + ", " + "" + x + ", " + null
+							+ ", " + null + ", " + null + ")";
+
 					stmt.executeUpdate(sql);
-				}
-				else if (cmd.equals("DROP")) {
-					sql = "DELETE FROM dtables WHERE "
-							+ "tname = '" + tname + "'"
-							+ "AND nodedriver = '" + (nodeAL.get(x)).getDriver() + "' "
-							+ "AND nodeurl = '" + (nodeAL.get(x)).getHostName() + "' "
-							+ "AND nodeuser = '" + (nodeAL.get(x)).getUserName() + "' "
-							+ "AND nodepasswd = '" + (nodeAL.get(x)).getPassword() + "' "
-							//+ "AND partmtd = " + -1 + "'"
-							+ "AND nodeid = "+ x + ""
-							//+ "AND partcol = '" + null + "'"
-							//+ "AND partparam1 = '" + null + "'"
-							//+ "AND partparam2 = '"  + null
+				} else if (cmd.equals("DROP")) {
+					sql = "DELETE FROM dtables WHERE " + "tname = '" + tname + "'" + "AND nodedriver = '"
+							+ (nodeAL.get(x)).getDriver() + "' " + "AND nodeurl = '" + (nodeAL.get(x)).getHostName()
+							+ "' " + "AND nodeuser = '" + (nodeAL.get(x)).getUserName() + "' " + "AND nodepasswd = '"
+							+ (nodeAL.get(x)).getPassword() + "' "
+							// + "AND partmtd = " + -1 + "'"
+							+ "AND nodeid = " + x + ""
+							// + "AND partcol = '" + null + "'"
+							// + "AND partparam1 = '" + null + "'"
+							// + "AND partparam2 = '" + null
 							+ "";
-					
+
 					stmt.executeUpdate(sql);
 				}
-				
-				//stmt.executeQuery(sql);
-				//stmt.executeUpdate(sql);
-				
+
+				// stmt.executeQuery(sql);
+				// stmt.executeUpdate(sql);
+
 			}
 
-			//rs.close();
+			// rs.close();
 			stmt.close();
 			conn.close();
-			System.out.println("[" + catalogHostName.substring(0, (catalogHostName.length() -1 )) + "]: Catalog Updated");
+			System.out
+					.println("[" + catalogHostName.substring(0, (catalogHostName.length() - 1)) + "]: Catalog Updated");
 		} catch (SQLException se) {
 			// Handle errors for JDBC
-			//se.printStackTrace();
-			System.out.println("[" + catalogHostName.substring(0, (catalogHostName.length() -1 )) + "]: catalog update failed");
+			// se.printStackTrace();
+			System.out.println(
+					"[" + catalogHostName.substring(0, (catalogHostName.length() - 1)) + "]: catalog update failed");
 		} catch (Exception e) {
 			// Handle errors for Class.forName
-			//e.printStackTrace();
-			System.out.println("[" + catalogHostName.substring(0, (catalogHostName.length() -1 )) + "]: catalog update failed");
+			// e.printStackTrace();
+			System.out.println(
+					"[" + catalogHostName.substring(0, (catalogHostName.length() - 1)) + "]: catalog update failed");
 		} finally {
 			// finally block used to close resources
 			try {
@@ -532,69 +564,86 @@ public class Main {
 				se.printStackTrace();
 			} // end finally try
 		} // end try
-		//System.out.println("Goodbye!");
-		
+			// System.out.println("Goodbye!");
+
 	}
+
 	
+	/* Function parseTname
+	 * Parameter: (String) sql : sql query to extract information from
+	 * 
+	 * Description: gets the table name in query
+	 * 
+	 * Returns: (String) : The table name
+	*/
 	private static String parseTname(String sql) {
 		String ret = "";
-		//String[] ret = str.split("name |field |grade ");
-		
+		// String[] ret = str.split("name |field |grade ");
+
 		char ch;
 		String word = "";
 		int control = 0;
-		
-		for(int x = 0; x < sql.length();x++) {
+
+		for (int x = 0; x < sql.length(); x++) {
 			ch = sql.charAt(x);
-			//word = word + ch;
-			
+			// word = word + ch;
+
 			if (control == 1) {
-				if(ch == '(' || ch == ' ') {
+				if (ch == '(' || ch == ' ') {
 					control++;
-				}
-				else {
+				} else {
 					ret = ret + ch;
 				}
-			}
-			else if (control == 0 && word.equals("TABLE")) {
+			} else if (control == 0 && word.equals("TABLE")) {
 				control++;
-			}
-			else if(ch == ' ') {
+			} else if (ch == ' ') {
 				word = "";
-			}
-			else {
+			} else {
 				word = word + ch;
 			}
 		}
-		
-		
-		
-		return(ret);
-	}
-	
-	public static String getCmd(String sql) {
-		String ret = "";
-		
-		char ch;
-		String word = "";
-		
-		for(int x = 0; x < sql.length();x++) {
-			ch = sql.charAt(x);
-			//word = word + ch;
-			
-			if (ch == ' ') {
-				x = sql.length() + 1;
-			}
-			else {
-				word = word + ch;
-			}
-		}
-		
-		ret = word;
-		
-		return(ret);
+
+		return (ret);
 	}
 
+	
+	/* Function getCmd
+	 * Parameter: (String) sql : sql query to extract information from
+	 * 
+	 * Description: gets the ddl command from a sql query.
+	 * 
+	 * Returns: (String) : The ddl command of the sql in regards to table, 
+	 * 						such as create or drop.
+	*/
+	public static String getCmd(String sql) {
+		String ret = "";
+
+		char ch;
+		String word = "";
+
+		for (int x = 0; x < sql.length(); x++) {
+			ch = sql.charAt(x);
+			// word = word + ch;
+
+			if (ch == ' ') {
+				x = sql.length() + 1;
+			} else {
+				word = word + ch;
+			}
+		}
+
+		ret = word;
+
+		return (ret);
+	}
+
+	/* Function doThread
+	 * Parameter: none
+	 * 
+	 * Description: runs multi threading.
+	 * 
+	 * Returns: void
+	*/
 	static void doThread() throws InterruptedException {
 		threadMessage("Starting MessageLoop thread");
 		long startTime = System.currentTimeMillis();
@@ -608,7 +657,7 @@ public class Main {
 		String user = null;
 		String pass = null;
 
-		//for (int i = 0; i < nthreads; i++) {
+		// for (int i = 0; i < nthreads; i++) {
 		for (int i = 0; (i < nthreads) && (i < nodeAL.size()); i++) {
 			driver = (nodeAL.get(i)).getDriver();
 			host = (nodeAL.get(i)).getHostName();
@@ -622,7 +671,7 @@ public class Main {
 
 		threadMessage("Waiting for all MessageLoop threads to finish");
 
-		//for (int i = 0; i < nthreads; i++) {
+		// for (int i = 0; i < nthreads; i++) {
 		for (int i = 0; (i < nthreads) && (i < nodeAL.size()); i++) {
 			tList[i].join();
 		}
@@ -636,7 +685,7 @@ public class Main {
 		String threadName = Thread.currentThread().getName();
 		System.out.format("%s: %s%n", threadName, message);
 	}
-
+	
 	private static class MessageLoop implements Runnable {
 
 		private String JDBC_DRIVER;
@@ -668,7 +717,7 @@ public class Main {
 					// Print a message
 					threadMessage(sqlAL.get(i));
 					runDDL(JDBC_DRIVER, DB_URL, USER, PASS, sqlAL.get(i));
-					
+
 				}
 			} catch (InterruptedException e) {
 				threadMessage("I wasn't done!");
